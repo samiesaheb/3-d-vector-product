ef8935e08970c381cb22db86cf411500fcb8ee2b
Samie Saheb <shs.priv@gmail.com>
2020-12-07 17:04:00 +0700

    Update vectorDotCross.java
    Removed the parameter 'input' from the CheckUIO method, moved the Scanner class for userInput and the var char input from main() into the CheckUIO method.

--- vectorDotCross.java
+++ vectorDotCross.java
@@ -16,12 +16,15 @@ public class Vector {
 
 
 	}
-	static void checkUIO(List<Integer> v_1, List<Integer> v_2, char input) {
+	static void checkUIO(List<Integer> v_1, List<Integer> v_2) {
 
 		while (true) {
-					if ((input == 'd') || (input == 'D')) {
+		System.out.println("Find dot product ('d/D') or cross product ('c/C')?\n");
+		Scanner userInput = new Scanner(System.in);
+		char input = userInput.next().charAt(0);
+		if ((input == 'd') || (input == 'D')) {
 			int dotProd = ((v_1.get(0)*v_2.get(0))+((v_1.get(1)*v_2.get(1)))+(v_1.get(2)*v_2.get(2)));
-			System.out.println("(v1 . v2) = " + dotProd);
+			System.out.println("\n(v1 . v2) = " + dotProd+"\n");
 			break;
 		}
 		else if ((input == 'c') || (input == 'C')) {
@@ -30,27 +33,24 @@ public class Vector {
 			int k = ((v_1.get(0)*v_2.get(1))-(v_1.get(1)*v_2.get(0)));
 			List<Integer> crossProd = new ArrayList<>();
 			crossProd.add(i);crossProd.add(j);crossProd.add(k);
-			System.out.println("(v1 x v2) = " + crossProd);
+			System.out.println("\n(v1 x v2) = " + crossProd + "\n");
 			break;
 		}
 		else {
 
-			System.out.println("Wrong input, try again");
+			System.out.println("\nWrong input, try again\n");
 			continue;
 		}
 		}
 
 	}
 	public static void main(String[] args){
-		
+
 		List<Integer> v1 = new ArrayList<>();
 		List<Integer> v2 = new ArrayList<>();
 		vectorIO(v1);
 		vectorIO(v2);
-		System.out.println("Find dot product ('d/D') or cross product ('c/C')?");
-		Scanner userInput = new Scanner(System.in);
-		char uInput = userInput.next().charAt(0);
-		checkUIO(v1,v2,uInput);
+		checkUIO(v1,v2);
 
 
 	}
